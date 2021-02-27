0cb9b67513fd255537fdbd6335c87b0d307fd4f9
Claudio René Aránguiz Herrera <claudioarnguiz@yahoo.com>
2021-02-27 16:54:10 -0300

    funcion de validacion backend funcional

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,20 @@ const {Router}  =  require('express');
 const {Mensaje, Coment} = require("../model/model");
 const rou = Router();    
 
+function checkin(req, res, next) {
+    console.log("Checkin antes del if");
+    console.log(req.body);
+    if (req.body.owner =="") {
+      console.log('Error por anonimato');
+      return res.redirect('/');
+    }
+    if (req.body.message =="") {
+        console.log('Error por mensaje vacio');
+        return res.redirect('/');        
+    }
+    next();
+ }
+
 rou
 .get("/", async (req,res)=>{    
     const mensajes = await Mensaje.findAll({
@@ -9,11 +23,10 @@ rou
             model: Coment
         }]
     });
-    console.log(mensajes);
     res.render("index",{mensajes: mensajes});   
 })
 
-.post('/', async (req,res)=>{
+.post('/',checkin, async (req,res)=>{
     const new_message = await Mensaje.create(req.body);
     res.redirect('/');  
 });
@@ -23,7 +36,7 @@ rou
     const comentarios = await Coment.findAll();
     res.render("index",{comentarios: comentarios});   })
 
-.post('/coment', async (req,res)=>{
+.post('/coment',checkin, async (req,res)=>{
     console.log(req.body);
     const new_coment = await Coment.create(req.body);
     res.redirect('/');  });
